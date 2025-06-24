826822ca592a00a495a3dddd57a6e9b02d2da750
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-06-24 15:06:56 +0200

    Visuals of create account

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -13,6 +13,7 @@ type Service = {
 function Home() {
   const navigate = useNavigate();
   const [existingServices, setExistingServices] = useState<Service[]>([]);
+  const [loginPanel, setLoginPanel] = useState(true);
 
   // Partie login
   const [mail, setMail] = useState("");
@@ -93,93 +94,128 @@ function Home() {
       {/* Container de la modale (c.a.d. l'écran) */}
       <div className="fixed inset-0 flex items-center justify-center z-20">
         {/* La modale elle même*/}
-        <div className="bg-white rounded-3xl shadow-md w-3/4 h-2/3 lg:w-[80rem] lg:h-[30rem] max-w-9/10 p-5">
-          <h1 className="text-center font-bold font-kalam text-5xl md:mb-10 md:mt-4">
+        <div className="bg-white rounded-3xl shadow-md w-3/4 lg:w-[80rem] max-w-9/10 p-5">
+          <h1 className="text-center font-bold font-kalam text-5xl md:mb-15 md:mt-4 mb-8 mt-6">
             idealy
           </h1>
 
-          <section className="flex flex-col-reverse md:flex-row justify-evenly items-center">
-            <p className=" mt-20 md:mt-0">Et coucou !</p>
-
-            {/* <section className="flex flex-col justify-center items-center gap-5">
-              <p className="mt-5 md:mt-0">Login</p>
-              <input
-                value={mail}
-                placeholder="mail"
-                onChange={(e) => setMail(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <input
-                value={password}
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <button
-                type="button"
-                onClick={handleLogin}
-                className="bg-greenButton text-black rounded-3xl px-9"
-              >
-                Se connecter
-              </button>
-            </section> */}
-
-            <section className="flex flex-col justify-center items-center gap-5">
-              <p className="mt-5 md:mt-0">Créer un compte</p>
-              <input
-                value={firstname}
-                placeholder="Firstname"
-                onChange={(e) => setFirstname(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <input
-                value={lastname}
-                placeholder="Lastname"
-                onChange={(e) => setLastname(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <input
-                value={mail}
-                placeholder="mail"
-                type="email"
-                onChange={(e) => setMail(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <input
-                value={password}
-                type="password"
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <input
-                value={picture}
-                placeholder="Profil picture URL"
-                onChange={(e) => setPicture(e.target.value)}
-                className="rounded-3xl text-center bg-greyBackground"
-              />
-              <select
-                id="service"
-                name="service"
-                value={service_id}
-                onChange={(e) => setService_id(Number(e.target.value))}
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >
-                <option value="">Choisir un service</option>
-                {existingServices.map((service) => (
-                  <option key={service.id} value={service.id}>
-                    {service.statut}
-                  </option>
-                ))}
-              </select>
-              <button
-                type="button"
-                onClick={handleCreateAccount}
-                className="bg-yellowButton text-black rounded-3xl px-9"
-              >
-                Créer mon compte
-              </button>
+          <section className="flex flex-col-reverse md:flex-row justify-evenly align-bottom">
+            <section className="flex flex-col gap-4 text-left max-md:hidden">
+              <p className="text-2xl font-bold">
+                idealy, qu'est ce que c'est ?
+              </p>
+              <p>
+                {" "}
+                <span className="text-yellowButton text-xl"> ● </span> Rédigez
+                et soumettez vos idées en ligne
+              </p>
+              <p>
+                {" "}
+                <span className="text-yellowButton text-xl"> ● </span>{" "}
+                Consultez, commentez, votez pour celles des autres contributeurs
+              </p>
+              <p>
+                {" "}
+                <span className="text-yellowButton text-xl"> ● </span> Suivez en
+                temps réel la prise de décision de celles-ci
+              </p>
             </section>
+
+            {loginPanel ? (
+              <section className="flex flex-col items-center gap-2">
+                <input
+                  value={mail}
+                  placeholder="Mail"
+                  type="mail"
+                  onChange={(e) => setMail(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground w-60 focus:outline-none py-1"
+                />
+                <input
+                  value={password}
+                  placeholder="Mot de passe"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground w-60 focus:outline-none py-1"
+                />
+                <button
+                  type="button"
+                  onClick={handleLogin}
+                  className="bg-greenButton text-black rounded-3xl w-50 py-2 mt-10"
+                >
+                  Se connecter
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setLoginPanel(!loginPanel)}
+                  className="bg-redButton text-black rounded-3xl w-50 py-2 mb-10"
+                >
+                  Je n'ai pas de compte
+                </button>
+              </section>
+            ) : (
+              <section className="flex flex-col justify-center items-center gap-2">
+                <input
+                  value={firstname}
+                  placeholder="Votre prénom"
+                  onChange={(e) => setFirstname(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground focus:outline-none w-60 py-1"
+                />
+                <input
+                  value={lastname}
+                  placeholder="Votre nom de famille"
+                  onChange={(e) => setLastname(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground focus:outline-none w-60 py-1"
+                />
+                <input
+                  value={mail}
+                  placeholder="Votre adresse mail"
+                  type="email"
+                  onChange={(e) => setMail(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground focus:outline-none w-60 py-1"
+                />
+                <input
+                  value={password}
+                  type="password"
+                  placeholder="Votre mot de passe"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground focus:outline-none w-60 py-1"
+                />
+                <input
+                  value={picture}
+                  placeholder="Lien de l'image de profil"
+                  onChange={(e) => setPicture(e.target.value)}
+                  className="rounded-3xl text-center bg-greyBackground focus:outline-none w-60 py-1"
+                />
+                <select
+                  id="service"
+                  name="service"
+                  value={service_id}
+                  onChange={(e) => setService_id(Number(e.target.value))}
+                  className="bg-greyBackground rounded-3xl focus:outline-none w-60 text-center py-1"
+                >
+                  <option value="">Choisir un service</option>
+                  {existingServices.map((service) => (
+                    <option key={service.id} value={service.id}>
+                      {service.statut}
+                    </option>
+                  ))}
+                </select>
+                <button
+                  type="button"
+                  onClick={handleCreateAccount}
+                  className="bg-greenButton text-black rounded-3xl w-50 py-2 mt-10"
+                >
+                  Créer mon compte
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setLoginPanel(!loginPanel)}
+                  className="bg-redButton text-black rounded-3xl w-50 py-2 mb-10"
+                >
+                  Retour menu connexion
+                </button>
+              </section>
+            )}
           </section>
         </div>
       </div>
