a5a49fba681de8495d62ae3fd63d6975f4391fef
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-06-24 14:04:12 +0200

    Minimal functionality added

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,31 @@
 // client/src/pages/Home.tsx
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { useLogin } from "../context/AuthContext";
 import Parcourir from "./Parcourir";
 
+type Service = {
+  id: number;
+  statut: string;
+};
+
 function Home() {
+  const navigate = useNavigate();
+  const [existingServices, setExistingServices] = useState<Service[]>([]);
+
+  // Partie login
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useLogin();
-  const navigate = useNavigate();
+
+  // Partie création de compte
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [picture, setPicture] = useState(
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+  );
+  const [service_id, setService_id] = useState(1);
 
   const handleLogin = async () => {
     try {
@@ -19,6 +35,52 @@ function Home() {
       // Gérer l'erreur (afficher un message à l'utilisateur par exemple)
     }
   };
+
+  const handleCreateAccount = async () => {
+    if (!firstname || !lastname || !mail || !password || !service_id) {
+      alert("Merci de remplir tous les champs.");
+      return;
+    }
+
+    try {
+      const userData = {
+        firstname,
+        lastname,
+        mail,
+        password,
+        picture,
+        isAdmin: false,
+        service_id,
+      };
+
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/users`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(userData),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Erreur lors de la création du compte.");
+      }
+
+      await handleLogin();
+    } catch (error) {
+      console.error("Erreur de création :", error);
+      alert("Une erreur est survenue lors de l'inscription.");
+    }
+  };
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/services`)
+      .then((response) => response.json())
+      .then((data) => {
+        setExistingServices(data);
+      })
+      .catch((error) => console.error("Erreur:", error));
+  }, []);
 
   return (
     <section className="min-h-lvh relative">
@@ -39,7 +101,7 @@ function Home() {
           <section className="flex flex-col-reverse md:flex-row justify-evenly items-center">
             <p className=" mt-20 md:mt-0">Et coucou !</p>
 
-            <section className="flex flex-col justify-center items-center gap-5">
+            {/* <section className="flex flex-col justify-center items-center gap-5">
               <p className="mt-5 md:mt-0">Login</p>
               <input
                 value={mail}
@@ -59,6 +121,63 @@ function Home() {
                 className="bg-greenButton text-black rounded-3xl px-9"
               >
                 Se connecter
+              </button>
+            </section> */}
+
+            <section className="flex flex-col justify-center items-center gap-5">
+              <p className="mt-5 md:mt-0">Créer un compte</p>
+              <input
+                value={firstname}
+                placeholder="Firstname"
+                onChange={(e) => setFirstname(e.target.value)}
+                className="rounded-3xl text-center bg-greyBackground"
+              />
+              <input
+                value={lastname}
+                placeholder="Lastname"
+                onChange={(e) => setLastname(e.target.value)}
+                className="rounded-3xl text-center bg-greyBackground"
+              />
+              <input
+                value={mail}
+                placeholder="mail"
+                type="email"
+                onChange={(e) => setMail(e.target.value)}
+                className="rounded-3xl text-center bg-greyBackground"
+              />
+              <input
+                value={password}
+                type="password"
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="rounded-3xl text-center bg-greyBackground"
+              />
+              <input
+                value={picture}
+                placeholder="Profil picture URL"
+                onChange={(e) => setPicture(e.target.value)}
+                className="rounded-3xl text-center bg-greyBackground"
+              />
+              <select
+                id="service"
+                name="service"
+                value={service_id}
+                onChange={(e) => setService_id(Number(e.target.value))}
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                <option value="">Choisir un service</option>
+                {existingServices.map((service) => (
+                  <option key={service.id} value={service.id}>
+                    {service.statut}
+                  </option>
+                ))}
+              </select>
+              <button
+                type="button"
+                onClick={handleCreateAccount}
+                className="bg-yellowButton text-black rounded-3xl px-9"
+              >
+                Créer mon compte
               </button>
             </section>
           </section>
