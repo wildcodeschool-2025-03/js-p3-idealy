111e57a6d6049397f6c4270cae41aac570a20b2d
Daviddemarville <de.marville.david@gmail.com>
2025-06-26 15:04:00 +0200

    add fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,16 @@ import { Outlet } from "react-router";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./App.css";
-import AuthGuard from "./components/AuthGuard";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
-      <AuthGuard>
-        <Header />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </AuthGuard>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </AuthProvider>
   );
 }
